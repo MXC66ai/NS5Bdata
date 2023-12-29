c9ba8cd3815a5e6bd6af5b2c3bb142287a3eff73
MXC66ai <108852007+MXC66ai@users.noreply.github.com>
2023-12-30 02:44:55 +0800

    Update 3tyv.sh

--- NewComplex/Palm1/3tyv.sh
+++ NewComplex/Palm1/3tyv.sh
@@ -16,11 +16,3 @@ for i in {1..157} ; do
   cd ~/AutoDockVina/autodock_vina_1_1_2_linux_x86/bin
   ./vina --receptor /home/smbu/0232/3tyv_A_nolig.pdbqt --ligand /home/smbu/0232/CHEMBL1148540/CHEMBL1148540_${i}.pdbqt  --center_x 25.191 --center_y  44.792 --center_z  44.020  --size_x 15  --size_y 15  --size_z 15 --out /home/smbu/0232/CHEMBL1148540/3tyv/CHEMBL1148540_${i}.pdbqt --log /home/smbu/0232/CHEMBL1148540/3tyv/CHEMBL1148540_${i}.log --cpu 8
 done
-python3 /home/smbu/0232/affinity_extract.py
-#! /bin/bash -i
-for i in {1..134} ; do
-  cd ~/AutoDockVina/autodock_vina_1_1_2_linux_x86/bin
-  ./vina --receptor /home/smbu/0232/3tyv_A_nolig.pdbqt --ligand /home/smbu/0232/CHEMBL1149391/CHEMBL1149391_${i}.pdbqt  --center_x 25.191 --center_y  44.792 --center_z  44.020  --size_x 15  --size_y 15  --size_z 15 --out /home/smbu/0232/CHEMBL1149391/3tyv/CHEMBL1149391_${i}.pdbqt --log /home/smbu/0232/CHEMBL1149391/3tyv/CHEMBL1149391_${i}.log --cpu 8
-done
-python3 /home/smbu/0232/affinity_extract.py
-
